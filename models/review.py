ed22cba71bdae61de0a47cacd08c691e782a1e52
mosweubarona <mosweubarona@gmail.com>
2022-09-16 03:47:41 -0700

    m re order base and basemodel

--- models/review.py
+++ models/review.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines the Review class."""
-from models.base_model import Base
 from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import String
